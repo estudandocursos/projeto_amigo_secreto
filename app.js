544cf9dfa95b8c234a3a2bcf1627c0d736a20c03
Estudando Cursos <estudandocursos29@gmail.com>
2025-08-13 20:38:07 -0300

    Inserido Notas e funções no js

--- app.js
+++ app.js
@@ -1,1 +1,60 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é 
+// fortalecer suas habilidades em lógica de programação. 
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+
+
+let campoDigitado = document.querySelector('.input-name input').value;
+let listaNomes = [];
+
+
+
+
+
+
+
+
+
+// FUNCTIONS
+
+function mostrarNomes(tag, texto){
+
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+
+function adcionarAmigos() {
+
+    if (campoDigitado.value == " ") {
+        alert('Não foi digitado nenhum nome. Digite algum nome por favor!');
+    } else {
+        
+    }
+
+    //listaNomes.push;
+}
+
+function sortearAmigo() {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
