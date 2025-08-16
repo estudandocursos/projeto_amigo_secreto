8aec184a0016d1b938ac70df5009a7122d1a0d78
Estudando Cursos <estudandocursos29@gmail.com>
2025-08-16 16:20:42 -0300

    Modificando função adcionarAmigos e info.txt

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 // Aqui você deverá desenvolver a lógica para resolver o problema.
 
 
-let campoDigitado = document.querySelector('.input-name input').value;
-let listaNomes = [];
 
-let tabelaAmigos = document.querySelector('.name-list ul');
 let resultado = document.querySelector('result-list ul');
+const meuBotao = document.getElementsByClassName('button-add');
+
+//meuBotao.addEventListener('click', adicionarAmigo()); 
 
 
 
@@ -22,16 +22,62 @@ function mostrarNomes(tag, texto){
     campo.innerHTML = texto;
 }
 
+function limparCampos(){
 
-function adcionarAmigos() {
+    campoDigitado = document.querySelector('amigo');
+    campoDigitado.value = "";
+
+}
+
+function adicionarAmigo() {
+   
+    let meuArray = []; //array para receber as posições advindas do input
+
+    //obtendo o elemento do campo imput para capturar os dados digitados
+    let textoDigitado = document.getElementById('amigo'); 
+
+    //obter o elemento onde será inserido os input dentro da lista não ordenada
+    let tabelaAmigos = document.getElementById('lista-amigos');
+    
+    textoDigitado
+
+
+
+
+
+
+    
+    for(var i=0; i< meuArray.length; i++){
+        meuArray.push(textoDigitado.value);
+        console.log(meuArray); 
+}
+    
+    //meuArray.push(meuInput.value);
+    //console.log(meuArray); 
+
+    
+   
+ /**/
+
+    /*
+    let listaNomes = [];
+    let campoDigitado = document.querySelector('amigo');
+
+    let campo = campoDigitado.value;
+    //let tabelaAmigos = document.querySelector('.name-list ul');
 
     if (campoDigitado.value == " ") {
+
         alert('Não foi digitado nenhum nome. Por favor, insira um nome!');
+
     } else {
         
+        listaNomes.push(campo);
+        console.log(listaNomes);
+        limparCampos();
     }
 
-    //listaNomes.push;
+    */
 }
 
 function sortearAmigo() {
